7c8dc94d1db648c100ac64ca003eed1a13584a47
Bindiya53 <bindiyatandel.dcs22n@vnsgu.ac.in>
2022-11-09 15:44:23 +0530

    Added code

---
+++ ZWalks.API/Domain/Walk.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZWalks.API.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
